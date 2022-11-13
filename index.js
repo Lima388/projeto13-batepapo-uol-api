12c7ef8fe9cc4c99706be10ad08589345ec170c9
Rafhael <rafhael.cancado@gmail.com>
2022-11-13 04:52:34 -0300

    feat: add status update and idle user removal

--- index.js
+++ index.js
@@ -129,4 +129,46 @@ app.get("/messages", async (req, res) => {
   }
 });
 
+app.post("/status", async (req, res) => {
+  const user = req.header("User");
+  try {
+    const userSignedIn =
+      (await db.collection("participants").find({ name: user }).count()) > 0;
+    if (!userSignedIn) {
+      throw new Exception();
+    }
+
+    await db.collection("participants").update(
+      {
+        name: user,
+      },
+      {
+        $set: { lastStatus: Date.now() },
+      }
+    );
+    res.sendStatus(200);
+  } catch {
+    res.sendStatus(404);
+  }
+});
+
+setInterval(() => {
+  removeIdleParticipants();
+}, 15000);
+
+async function removeIdleParticipants() {
+  const participants = await db.collection("participants").find().toArray();
+  for (let i = 0; i < participants.length; i++) {
+    if (Date.now() - participants[i].lastStatus > 10000) {
+      db.collection("participants").deleteOne({ name: participants[i].name });
+      db.collection("messages").insertOne({
+        from: participants[i].name,
+        to: "Todos",
+        text: "sai da sala...",
+        type: "status",
+        time: dayjs().format("HH:mm:ss"),
+      });
+    }
+  }
+}
 app.listen(5000, () => console.log("Server running in port 5000"));
